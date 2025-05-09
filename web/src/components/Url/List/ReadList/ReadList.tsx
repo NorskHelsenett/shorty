f3b4d911bb2a679f3cb4470d11280af7aa3d4b65
abjerke <anne.lise.bjerkestrand@nhn.no>
2025-05-09 13:21:07 +0200

    refactor[readList]: deleted unused isAdmin

--- web/src/components/Url/List/ReadList/ReadList.tsx
+++ web/src/components/Url/List/ReadList/ReadList.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { QrData, UrlData } from '../../../../data/Types';
-import 'primeicons/primeicons.css';
-import './ReadList.css';
-import '../List.css';
-import { useAdminContext } from '../../../../Hooks/authAdminContext';
+import React from "react";
+import { QrData, UrlData } from "../../../../data/Types";
+import "primeicons/primeicons.css";
+import "./ReadList.css";
+import "../List.css";
+import { useAdminContext } from "../../../../Hooks/authAdminContext";
 interface ReadOnlyRowsProps {
   data: UrlData;
   index: number; // position to elements in array (data)
@@ -12,9 +12,13 @@ interface ReadOnlyRowsProps {
   onQrClick: (data: QrData) => void;
 }
 
-const ReadOnlyRows: React.FC<ReadOnlyRowsProps> = ({ data, index, onEdit, onDelete, onQrClick }) => {
-  const isAdmin = useAdminContext();
-
+const ReadOnlyRows: React.FC<ReadOnlyRowsProps> = ({
+  data,
+  index,
+  onEdit,
+  onDelete,
+  onQrClick,
+}) => {
   var modify = data.modify;
   //console.log('admin:', isAdmin, 'owner:', data.owner, 'modify:', modify, 'data.Modify', data.modify);
 
@@ -36,21 +40,27 @@ const ReadOnlyRows: React.FC<ReadOnlyRowsProps> = ({ data, index, onEdit, onDele
         <div className="list-item__actions">
           <button
             data-tooltip-id="edit-tooltip"
-            data-tooltip-content={modify ? 'Edit line' : `owner: ${data.owner}`}
+            data-tooltip-content={modify ? "Edit line" : `owner: ${data.owner}`}
             onClick={() => onEdit(index)}
             disabled={!modify}
           >
-            <i className={`pi ${modify ? ' pi-pencil' : 'pi-ban'}`}></i>
+            <i className={`pi ${modify ? " pi-pencil" : "pi-ban"}`}></i>
           </button>
           <button
             data-tooltip-id="delete-tooltip"
-            data-tooltip-content={modify ? 'Delete line' : `owner: ${data.owner}`}
+            data-tooltip-content={
+              modify ? "Delete line" : `owner: ${data.owner}`
+            }
             onClick={() => onDelete(data.path)}
             disabled={!modify}
           >
-            <i className={`pi ${modify ? 'pi-trash' : 'pi-ban'}`}></i>
+            <i className={`pi ${modify ? "pi-trash" : "pi-ban"}`}></i>
           </button>
-          <button data-tooltip-id="qrcode-tooltip" data-tooltip-content="Show QR-code" onClick={handleOnClick}>
+          <button
+            data-tooltip-id="qrcode-tooltip"
+            data-tooltip-content="Show QR-code"
+            onClick={handleOnClick}
+          >
             <i className={`pi pi-qrcode`}></i>
           </button>
         </div>
