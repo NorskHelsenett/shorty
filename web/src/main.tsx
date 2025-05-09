a443349f26dda86a58069f126a4d28e93d423673
abjerke <anne.lise.bjerkestrand@nhn.no>
2025-05-09 14:36:39 +0200

    fix[web main]: Index.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,16 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { AuthProvider } from 'react-oauth2-code-pkce';
-import { authConfig } from './Service/Auth/authService.ts';
-import { AdminProvider } from './Hooks/authAdminContext.tsx';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import './index.css';
-import './App.css';
-import './index.css';
-import AdminPage from './Pages/AdminPage.tsx';
-import HomePage from './Pages/HomePage.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { AuthProvider } from "react-oauth2-code-pkce";
+import { authConfig } from "./Service/Auth/authService.ts";
+import { AdminProvider } from "./Hooks/authAdminContext.tsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./Index.css";
+import AdminPage from "./Pages/AdminPage.tsx";
+import HomePage from "./Pages/HomePage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider authConfig={authConfig}>
       <AdminProvider>
@@ -22,5 +21,5 @@ createRoot(document.getElementById('root')!).render(
         </Router>
       </AdminProvider>
     </AuthProvider>
-  </StrictMode>,
+  </StrictMode>
 );
