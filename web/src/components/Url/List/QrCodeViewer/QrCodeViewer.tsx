5927b15045391f35c2f74e9253745f1410d902d5
abjerke <anne.lise.bjerkestrand@nhn.no>
2025-05-09 12:17:26 +0200

    feat: build-test-deploy workflow

--- web/src/components/Url/List/QrCodeViewer/QrCodeViewer.tsx
+++ web/src/components/Url/List/QrCodeViewer/QrCodeViewer.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Tooltip } from 'react-tooltip';
-import './QrCodeViewer.css';
-import { QRCodeCanvas } from 'qrcode.react';
-import { QrData } from '../../../../data/Types';
+import React from "react";
+import { Tooltip } from "react-tooltip";
+import "./QrCodeViewer.css";
+import { QRCodeCanvas } from "qrcode.react";
+import { QrData } from "../../../../data/Types";
 
 interface QrCodeViewerProps {
   imagePath: QrData;
@@ -10,37 +10,56 @@ interface QrCodeViewerProps {
   isVisible: boolean;
 }
 
-const qrCodeViewer: React.FC<QrCodeViewerProps> = ({ imagePath, closeQr, isVisible }) => {
+const qrCodeViewer: React.FC<QrCodeViewerProps> = ({
+  imagePath,
+  closeQr,
+  isVisible,
+}) => {
   const handleClose = () => {
     closeQr();
   };
 
   const downloadQrCode = () => {
-    console.log('downloadQrCode');
-    const canvas = document.querySelector('#qrcode-canvas') as HTMLCanvasElement;
-    if (!canvas) throw new Error('<canvas> not found int the DOM');
+    console.log("downloadQrCode");
+    const canvas = document.querySelector(
+      "#qrcode-canvas"
+    ) as HTMLCanvasElement;
+    if (!canvas) throw new Error("<canvas> not found int the DOM");
 
-    const pngUrl = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
-    const downloadLink = document.createElement('a');
+    const pngUrl = canvas
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    const downloadLink = document.createElement("a");
     downloadLink.href = pngUrl;
-    downloadLink.download = 'QR code.png';
+    downloadLink.download = "QR code.png";
     document.body.appendChild(downloadLink);
     downloadLink.click();
     document.body.removeChild(downloadLink);
   };
 
-  const imageUrlPath = 'k.nhn.no/' + imagePath;
-  const isNhnUrl = imagePath.url.includes('nhn');
+  const imageUrlPath = "k.nhn.no/" + imagePath;
+  console.log("imagePath", imagePath);
+  const isNhnUrl = imagePath.url.includes("nhn");
 
   return (
     <>
-      <dialog className={'qrcode-dialog'} id="dialog" open={isVisible}>
+      <dialog className={"qrcode-dialog"} id="dialog" open={isVisible}>
         <div className="dialog-buttons">
-          <button id="download" data-tooltip-id="downlaod-tooltip" data-tooltip-content="Download" onClick={downloadQrCode}>
-            {' '}
+          <button
+            id="download"
+            data-tooltip-id="downlaod-tooltip"
+            data-tooltip-content="Download"
+            onClick={downloadQrCode}
+          >
+            {" "}
             <i className="pi pi-file-o"></i>
           </button>
-          <button id="close" data-tooltip-id="close-tooltip" data-tooltip-content="Close" onClick={handleClose}>
+          <button
+            id="close"
+            data-tooltip-id="close-tooltip"
+            data-tooltip-content="Close"
+            onClick={handleClose}
+          >
             <i className="pi pi-times"></i>
           </button>
         </div>
@@ -49,8 +68,17 @@ const qrCodeViewer: React.FC<QrCodeViewerProps> = ({ imagePath, closeQr, isVisib
           value={imageUrlPath}
           id="qrcode-canvas"
           size={200}
-          level={'H'}
-          imageSettings={isNhnUrl ? { src: './public/nhnlogo.png', height: 40, width: 40, excavate: true } : undefined}
+          level={"H"}
+          imageSettings={
+            isNhnUrl
+              ? {
+                  src: "./public/nhnlogo.png",
+                  height: 40,
+                  width: 40,
+                  excavate: true,
+                }
+              : undefined
+          }
         />
       </dialog>
       <Tooltip id="downlaod-tooltip" />
