c0b103c23f4ce71a76fcd03b4a13efcadb39ca21
abjerke <anne.lise.bjerkestrand@nhn.no>
2025-05-13 19:57:39 +0200

    refactor: redirectUri /admin

--- web/src/Service/config.ts
+++ web/src/Service/config.ts
@@ -1,6 +1,7 @@
 
 export const AUTH_URL = import.meta.env.VITE_AUTH_URL;
 export const API_URL = import.meta.env.VITE_API_URL;
+export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
 
 
 import {TAuthConfig} from "react-oauth2-code-pkce"
@@ -9,6 +10,6 @@ export const AUTH_CONFIG: TAuthConfig = {
     clientId: "shortyfront",
     authorizationEndpoint: `${AUTH_URL}/dex/auth`,
     tokenEndpoint: `${AUTH_URL}/dex/token`,
-    redirectUri: "http://localhost:5173",
+    redirectUri: `${REDIRECT_URI}/admin`,
     scope: "openid profile email groups",
 }
