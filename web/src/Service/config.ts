909f8a1870299b40a3f84f3e01cd755a3128afdb
abjerke <anne.lise.bjerkestrand@nhn.no>
2025-05-14 07:33:15 +0200

    feat[web]: getEnv function

--- web/src/Service/config.ts
+++ web/src/Service/config.ts
@@ -1,7 +1,16 @@
+function getEnv(variable: string | undefined, name: string, defaultValue: string): string {
+    if (!variable){
+        console.warn(`Enveroment variable ${name} is not set; using standard value: ${defaultValue}`)
+        return defaultValue;
+    }
+    return variable;
+}
 
-export const AUTH_URL = import.meta.env.VITE_AUTH_URL;
-export const API_URL = import.meta.env.VITE_API_URL;
-export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+
+// get environment from terminal
+export const AUTH_URL = getEnv(import.meta.env.VITE_AUTH_URL, "VITE_AUTH_URL", "http://localhost:5556");
+export const API_URL = getEnv(import.meta.env.VITE_API_URL,"VITE_API_URL", "http://localhost:8880");
+export const REDIRECT_URI = getEnv(import.meta.env.VITE_REDIRECT_URI, "VITE_REDIRECT_URI", "http://localhost:5173/admin");
 
 
 import {TAuthConfig} from "react-oauth2-code-pkce"
