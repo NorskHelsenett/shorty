23f474f7324bbf329687118c2cd8cc7bfca33ebb
Håvard Elnan <havard.elnan@nhn.no>
2025-05-20 12:06:00 +0200

    fix(tsconfig): add DOM library to TypeScript configuration
    fix(vite.config): correct environment variable warning message and refactor allowed hosts retrieval

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,37 +1,44 @@
 /// <reference types="vite/client" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import * as process from 'process'
 
 function getEnv(variable: string | undefined, name: string, defaultValue: string[]): string[] {
     if (!variable){
-        console.warn(`Enveroment variable ${name} is not set; using standard value: ${defaultValue}`)
+        console.warn(`Environment variable ${name} is not set; using standard value: ${defaultValue}`)
         return defaultValue;
     }
+    console.info(`Environment variable ${name} is set; using value: ${variable.split(',')}`)
     return variable.split(',');
 }
 
-const ALLOWED_HOSTS = getEnv(import.meta.env.VITE_ALLOWED_HOSTS, "VITE_ALLOWED_HOSTS", ['127.0.0.1', 'localhost']);
+export default defineConfig(({ mode }) => {
+  // Load env files based on mode (.env, .env.production, etc)
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  // Get allowed hosts from environment variable or use default
+  const ALLOWED_HOSTS = getEnv(process.env.VITE_ALLOWED_HOSTS || env.VITE_ALLOWED_HOSTS, "VITE_ALLOWED_HOSTS", ['127.0.0.1', 'localhost'])
 
-export default defineConfig({
- base: "/",
- plugins: [react()],
- preview: {
-  port: 5173,
-  strictPort: true,
- },
- server: {
-  port: 5173,
-  strictPort: true,
-  host: '0.0.0.0',
-  origin: "http://localhost:5173",
-  hmr: {
-    clientPort: 5173,
-    protocol: 'ws',   
-  },
-  fs: {
-    strict: true,
-  },
-  allowedHosts: ALLOWED_HOSTS
- },
+  return {
+    base: "/",
+    plugins: [react()],
+    preview: {
+      port: 5173,
+      strictPort: true,
+    },
+    server: {
+      port: 5173,
+      strictPort: true,
+      host: '0.0.0.0',
+      origin: "http://localhost:5173",
+      hmr: {
+        clientPort: 5173,
+        protocol: 'ws',   
+      },
+      fs: {
+        strict: true,
+      },
+      allowedHosts: ALLOWED_HOSTS
+    },
+  }
 });
